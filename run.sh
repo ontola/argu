364cb71b0235594666bf8a4ddf3996d6fa72d557
Joep Meindertsma <joep@argu.co>
2017-02-27 15:25:18 +0100

    [FIX] Default port from 3031 to 3032, which prevents conflict with some Apple port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build . -t argu.proxy && docker run -p 3031:3031 -p 443:443 -v $(pwd)/www:/etc/nginx/www argu.proxy
+docker build . -t argu.proxy && docker run -p 3032:3032 -p 443:443 -v $(pwd)/www:/etc/nginx/www argu.proxy
