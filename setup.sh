405df7fd02b3e39c5d5143fa91a55206fb80a100
Fletcher91 <thom@argu.co>
2017-05-04 17:00:42 +0200

    [IMP] Use a different command for OSX and other operating systems

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-IP=$(ifconfig | awk '/broadcast/' | awk '/inet /{print $2}')
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  IP=$(ifconfig | awk '/broadcast/' | awk '/inet /{print $2}')
+# elif [[ "$OSTYPE" == "win32" ]]; then
+  # TODO
+else
+  IP=$(hostname -I | awk -F" " '{print $1}')
+fi
+
 echo "IP: $IP"
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=NL/ST=Utrecht/L=Utrecht/O=Argu/OU=Argu Development/CN=argu.local" -keyout ./ssl/nginx.key -out ./ssl/nginx.crt
