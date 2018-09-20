6adfce09eb94623e81b933b73a994c7b0c09805a
Arthur Dingemans <arthur@argu.co>
2018-09-20 12:33:29 +0200

    [DP][TS-47] Add legacy frontend functionality

--- spec/support/test_methods.rb
+++ spec/support/test_methods.rb
@@ -4,8 +4,9 @@ require 'mailcatcher/api'
 
 module TestMethods
   def as(actor, location: '/argu/freetown', password: 'password')
-    visit "https://app.argu.localtest#{location}"
-    login(actor, password) unless actor == :guest
+    visit "https://#{use_legacy_frontend? ? '' : 'app.'}argu.localtest#{location}"
+    return if actor == :guest
+    use_legacy_frontend? ? login_legacy(actor, password) : login(actor, password)
   end
 
   def login(email, password = 'password')
@@ -25,8 +26,20 @@ module TestMethods
     expect(page).not_to have_content 'inloggen of registreren'
   end
 
+  def login_legacy(email, password = 'password')
+    page.click_link('Log in')
+
+    expect(page).to have_css('.modal-opened')
+
+    within('.modal #new_user') do
+      fill_in 'user_email', with: email
+      fill_in 'user_password', with: password
+      click_button 'Log in'
+    end
+  end
+
   # Helper to aid in picking an option in a Selectize dropdown
-  def fill_in_select(scope = nil, with: nil, selector: nil)``
+  def fill_in_select(scope = nil, with: nil, selector: nil)
     select = lambda do
       input_field = find('.Select-control .Select-input input').native
       input_field.send_keys with
@@ -43,5 +56,18 @@ module TestMethods
 
   def verify_logged_in
     wait_for { page }.to have_css 'div[resource="https://app.argu.localtest/c_a"]'
+  end
+
+  def visit(url)
+    return super if use_legacy_frontend?
+    super(url.gsub('https://argu', 'https://app.argu'))
+  end
+
+  def use_legacy_frontend
+    @use_legacy_frontend = true
+  end
+
+  def use_legacy_frontend?
+    @use_legacy_frontend == true
   end
 end
--- test.sh
+++ test.sh
@@ -11,4 +11,7 @@ docker-compose up -d
 docker build .
 docker-compose up -d --force-recreate devproxy
 
+docker exec devproxy_argu_1 sed -i -e 's/staging.argu.co/argu.localtest/g' public/packs/manifest.json
+docker restart devproxy_argu_1
+
 echo "Don't forget to run 'bundle exec rake test:setup' if you haven't already"
