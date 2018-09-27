8c130bfbe94218eb8db61a13d7dba6cd83dca80a
Arthur Dingemans <arthur@argu.co>
2018-09-27 09:51:45 +0200

    [DP][TS-52] Add token specs

---
+++ spec/features/tokens/tokens_bearer_show_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+RSpec.describe 'Token bearer show', type: :feature do
+  context 'valid' do
+    let(:token) { '/tokens/valid_bearer_token' }
+
+    example 'new user visits bearer token' do
+      as(:guest, location: token)
+
+      fill_in_registration_form
+
+      expect_joined
+    end
+
+    example 'logged out user visits bearer token' do
+      as(:guest, location: token)
+
+      fill_in_login_form
+
+      expect_joined
+    end
+
+    example 'logged out member visits bearer token' do
+      as(:guest, location: token)
+
+      fill_in_login_form 'member@example.com'
+
+      expect_member_already
+    end
+
+    example 'user visits bearer token' do
+      as('user1@example.com')
+      visit "https://app.argu.localtest#{token}"
+
+      expect_joined
+    end
+
+    example 'member visits bearer token' do
+      as('member@example.com')
+      visit "https://app.argu.localtest#{token}"
+
+      expect_member_already
+    end
+  end
+
+  private
+
+  def expect_joined
+    wait(30).for(page).to have_content('Holland')
+    expect(page).to have_content("You have joined the group 'Members'")
+    # @todo verify favorite exists
+  end
+
+  def expect_member_already
+    wait(30).for(page).to have_content('Holland')
+    expect(page).to have_content('You are already member of this group')
+    # @todo verify favorite exists
+  end
+end
