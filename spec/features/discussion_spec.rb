ef0e2f636a2120564f194e5d2641659020b11464
Arthur Dingemans <arthur@argu.co>
2018-10-16 16:41:49 +0200

    [DP-15] Add update motion/question specs

--- spec/features/discussion_spec.rb
+++ spec/features/discussion_spec.rb
@@ -33,14 +33,32 @@ RSpec.describe 'Discussions', type: :feature do
     as 'member@example.com', location: '/argu/holland/discussions/new#new_question'
     expect_form('New challenge')
     fill_in_form
-    expect_new_content('Challenge', 'q')
+    expect_content('Challenge', 'q/64')
   end
 
   example 'Member posts a motion' do
     as 'member@example.com', location: '/argu/holland/discussions/new#new_motion'
     expect_form('New idea')
     fill_in_form
-    expect_new_content('Idea', 'm')
+    expect_content('Idea', 'm/64')
+  end
+
+  example 'staff updates a question' do
+    as 'staff@example.com', location: '/argu/q/35'
+    resource_selector('https://app.argu.localtest/argu/q/35/menus/actions').click
+    click_link 'Edit'
+    expect_form('Update')
+    fill_in_form
+    expect_content('Challenge', 'q/35', new: false)
+  end
+
+  example 'staff updates a motion' do
+    as 'staff@example.com', location: '/argu/m/32'
+    resource_selector('https://app.argu.localtest/argu/m/32/menus/actions').click
+    click_link 'Edit'
+    expect_form('Update')
+    fill_in_form
+    expect_content('Idea', 'm/32', new: false)
   end
 
   private
@@ -62,21 +80,25 @@ RSpec.describe 'Discussions', type: :feature do
   end
 
   def fill_in_form
-    fill_in 'http://schema.org/name', with: title
-    fill_in 'http://schema.org/text', with: content
+    fill_in 'http://schema.org/name', with: title, fill_options: {clear: :backspace}
+    fill_in 'http://schema.org/text', with: content, fill_options: {clear: :backspace}
     attach_file 'Content', File.absolute_path('spec/fixtures/profile_photo.png')
     # @todo upload cover_photo
     click_button 'Save'
   end
 
-  def expect_new_content(type, path) # rubocop:disable Metrics/AbcSize:
-    wait_for(page).to(
-      have_content("#{type} created successfully. it can take a few moments before it's visible on other pages")
-    )
+  def expect_content(type, path, new: true)
+    message =
+      if new
+        "#{type} created successfully. it can take a few moments before it's visible on other pages"
+      else
+        "#{type} saved successfully"
+      end
+    wait_for(page).to have_content message
     wait_for(page).to have_content(title)
     expect(page).to have_content(content)
-    resource_selector("https://app.argu.localtest/argu/#{path}/64/attachments", child: '.AttachmentPreview')
+    resource_selector("https://app.argu.localtest/argu/#{path}/attachments", child: '.AttachmentPreview')
     # @todo expect cover_photo
-    expect(current_path).to include("/argu/#{path}/")
+    expect(current_path).to include("/argu/#{path}")
   end
 end
