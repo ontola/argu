bb500e0e523a96d27337078fe6e8759319042e79
Arthur Dingemans <arthur@argu.co>
2018-10-16 15:02:26 +0200

    [DP-15] Update notification counts

--- spec/features/notifications_spec.rb
+++ spec/features/notifications_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe 'Notifications', type: :feature do
     as 'user1@example.com'
 
     within sidebar do
-      wait_for { count_bubble_count }.to have_content '2'
+      wait_for { count_bubble_count }.to have_content '4'
     end
     click_link 'Notifications'
 
     expect(page).to have_content 'first_name_32 last_name_32 posted a challenge in Freetown'
     expect(page).to have_content 'first_name_30 last_name_30 posted a idea in Freetown'
 
-    expect(page.all('[data-test="Notification-Unread"]').count).to eq(2)
+    expect(page.all('[data-test="Notification-Unread"]').count).to eq(4)
     page.all('[data-test="Notification-Unread"]').first.click
     wait_for { sidebar }.to have_content 'Notifications'
     within sidebar do
-      wait_for { count_bubble_count }.to have_content '1'
+      wait_for { count_bubble_count }.to have_content '3'
     end
 
-    expect(page.all('[data-test="Notification-Unread"]').count).to eq(1)
+    expect(page.all('[data-test="Notification-Unread"]').count).to eq(3)
   end
 end
