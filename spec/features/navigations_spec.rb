f52225743681a33f4f4a7b6937a5b4c213eeca0d
Arthur Dingemans <arthur@argu.co>
2018-11-06 15:11:18 +0100

    [DP-15] Reduce flakiness

--- spec/features/navigations_spec.rb
+++ spec/features/navigations_spec.rb
@@ -11,17 +11,17 @@ RSpec.describe 'Navigations', type: :feature do
     go_to_child 'Fg question title 7end'
     go_to_child 'Fg motion title 9end'
     go_to_child 'Fg argument title 6end'
-    expect(page).to have_content 'fg comment body 2end'
+    wait_for(page).to have_content 'fg comment body 2end'
   end
 
   example 'walk from argument to forum' do
     as :guest, location: '/argu/pro/41'
 
-    expect(page).to have_content 'fg comment body 2end'
+    wait_for(page).to have_content 'fg comment body 2end'
     go_to_parent 'Fg motion title 9end'
     go_to_parent 'Fg question title 7end'
     go_to_parent 'Freetown'
-    expect(page).to have_content 'Discussions'
+    wait_for(page).to have_content 'Discussions'
   end
 
   private
