5b08db0b0a3d967fb2e4641d400a86f03427a2fa
Arthur Dingemans <arthur@argu.co>
2019-03-25 13:43:49 +0100

    [DP-15] Add topic notification

--- spec/features/notifications_spec.rb
+++ spec/features/notifications_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Notifications', type: :feature do
     as 'user1@example.com'
 
     within navbar do
-      wait_for { count_bubble_count }.to have_content '4'
+      wait_for { count_bubble_count }.to have_content '5'
     end
     # @todo add link to notification in UI
     # click_link 'Notifications'
@@ -15,13 +15,14 @@ RSpec.describe 'Notifications', type: :feature do
 
     expect(page).to have_content 'first_name_32 last_name_32 posted a challenge in Freetown'
     expect(page).to have_content 'first_name_30 last_name_30 posted a idea in Freetown'
+    expect(page).to have_content 'first_name_57 last_name_57 posted a topic in Freetown'
 
-    expect(page.all('[data-test="Notification-Unread"]').count).to eq(4)
+    expect(page.all('[data-test="Notification-Unread"]').count).to eq(5)
     page.all('[data-test="Notification-Unread"]').first.click
     within navbar do
-      wait_for { count_bubble_count }.to have_content '3'
+      wait_for { count_bubble_count }.to have_content '4'
     end
 
-    expect(page.all('[data-test="Notification-Unread"]').count).to eq(3)
+    expect(page.all('[data-test="Notification-Unread"]').count).to eq(4)
   end
 end
