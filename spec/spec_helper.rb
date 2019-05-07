d753a73e7c9f838edabc4766326dfc09f6db45fc
Arthur Dingemans <arthur@argu.co>
2019-05-07 17:10:04 +0200

    [DP-15] MONKEY

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,27 @@ require 'support/docker_helper'
 require 'support/test_methods'
 require 'support/selectors'
 
+module Selenium
+  module WebDriver
+    class Options
+      # capybara/rspec installs a RSpec callback that runs after each test and resets
+      # the session - part of which is deleting all cookies. However the call to Chrome
+      # Webdriver to delete all cookies in Chrome 74 hangs when run in headless mode
+      # (the reasons for which are still unknown).
+      #
+      # Fortunately, the call to set a cookie is still functioning and we can rely
+      # on expired cookies being cleared by Chrome, so we iterate over all current
+      # cookies and set their expiry date to some time in the past - effectively
+      # deleting them.
+      def delete_all_cookies
+        all_cookies.each do |cookie|
+          add_cookie(name: cookie[:name], value: '', expires: Time.now - 1.second)
+        end
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   include DockerHelper
 
@@ -73,7 +94,8 @@ end
 Capybara.register_driver :selenium_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome('elementScrollBehavior' => 1)
   client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 90
+  client.open_timeout = 90
+  client.read_timeout = 90
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_preference('intl.accept_languages', 'en-US')
 
