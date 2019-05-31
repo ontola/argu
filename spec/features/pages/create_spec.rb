abef58d98bbeb0a664f9247195d25e527c8ce228
Arthur Dingemans <arthur@argu.co>
2019-05-31 10:32:27 +0200

    [DP-15] Add create page spec

---
+++ spec/features/pages/create_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Page create', type: :feature do
+  example 'user creates page' do
+    as 'user1@example.com'
+    click_application_menu_button('My Argu websites')
+    wait_for(page).to have_content 'Nog geen items'
+    click_link 'New website'
+    fill_in 'http://xmlns.com/foaf/0.1/name', with: 'My Website'
+    fill_in 'http://schema.org/description', with: 'About my website'
+    fill_in 'https://argu.co/ns/core#shortname', with: 'my_website'
+    check 'I accept the terms of use'
+    click_button 'Save'
+
+    wait(30).for(page).to have_current_path('/my_website/settings')
+    expect(navbar).to have_content 'My Website'
+    click_application_menu_button('My Argu websites')
+    wait_for(page).to have_content 'About my website'
+  end
+end
