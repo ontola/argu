677397d3205e17043cfa8c8bb5ac88c1b9aab7bd
Arthur Dingemans <arthur@argu.co>
2019-08-19 17:12:56 +0200

    [DP-15] Add apartment block to sync_tokens

--- sync_tokens.sh
+++ sync_tokens.sh
@@ -9,9 +9,11 @@ NEW_SERVICE_TOKEN=$(./generate_jwt.sh $SECRET_TOKEN -2 service)
 NEW_SERVICE_GUEST_TOKEN=$(./generate_jwt.sh $SECRET_TOKEN false guest afe)
 
 echo "Now run in Argu Rails console: "
-echo "token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu, User::SERVICE_ID, 'service', 1.year.from_now, false); token.update(token: '${NEW_SERVICE_TOKEN}')"
-echo "token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu_front_end, User::COMMUNITY_ID, 'service afe', 1.year.from_now, false); token.update(token: '${NEW_FRONTEND_TOKEN}')"
-echo "token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu_front_end, nil, 'guest afe', 1.year.from_now, false); token.update(token: '${NEW_SERVICE_GUEST_TOKEN}')"
+echo "Apartment::Tenant.each do"
+echo "  token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu, User::SERVICE_ID, 'service', 1.year.from_now, false); token.update(token: '${NEW_SERVICE_TOKEN}')"
+echo "  token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu_front_end, User::COMMUNITY_ID, 'service afe', 1.year.from_now, false); token.update(token: '${NEW_FRONTEND_TOKEN}')"
+echo "  token = Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.argu_front_end, nil, 'guest afe', 1.year.from_now, false); token.update(token: '${NEW_SERVICE_GUEST_TOKEN}')"
+echo "end"
 
 # Replace ENV vars
 sed -i "s/RAILS_OAUTH_TOKEN=.*/RAILS_OAUTH_TOKEN=${NEW_FRONTEND_TOKEN}/g" .env
