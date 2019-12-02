b600811b69ba845065a251a19ca6dd28ffb271f8
Arthur Dingemans <arthur@argu.co>
2019-12-02 14:42:39 +0100

    [DP-15] Update decisions spec

--- spec/features/decisions_spec.rb
+++ spec/features/decisions_spec.rb
@@ -10,7 +10,10 @@ RSpec.describe 'Decisions', type: :feature do
     select_radio 'Approve'
     fill_in 'http://schema.org/text', with: 'Reason for decision'
     click_button 'Save'
-    wait_for(page).to have_content 'Idea is approved', count: 2
+    wait_for(page).to have_snackbar 'Idea is approved'
+    within('#start-of-content') do
+      wait_for(page).to have_content 'Idea is approved'
+    end
     expect(page).to(have_content('Reason for decision'))
   end
 
@@ -21,7 +24,10 @@ RSpec.describe 'Decisions', type: :feature do
     select_radio 'Reject'
     fill_in 'http://schema.org/text', with: 'Reason for decision'
     click_button 'Save'
-    wait_for(page).to have_content 'Idea is rejected', count: 2
+    wait_for(page).to have_snackbar 'Idea is rejected'
+    within('#start-of-content') do
+      wait_for(page).to have_content 'Idea is rejected'
+    end
     expect(page).to(have_content('Reason for decision'))
   end
 end
