22385e5d8b68535266eba1f23752e553f38fad1d
Arthur Dingemans <arthur@argu.co>
2020-05-04 09:56:01 +0200

    core#5 Update Not Found spec

--- spec/features/not_found_spec.rb
+++ spec/features/not_found_spec.rb
@@ -5,7 +5,8 @@ require 'spec_helper'
 RSpec.describe 'Not found', type: :feature do
   example 'request non existing tenant' do
     as(:guest, location: '/wrong_tenant')
-    expect(page).to have_content('Not Found')
+    wait_for { page }.to have_content('This item is not found')
+    wait_for { page }.to have_button('Try again')
   end
 
   example 'request non existing path' do
