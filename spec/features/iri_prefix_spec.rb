503f22b1c45aae0781167b7a26a073ca664d8f09
Arthur Dingemans <arthur@argu.co>
2020-11-27 16:37:37 +0100

    core#15 Add spec for iriPrefix change

---
+++ spec/features/iri_prefix_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'iri prefix', type: :feature do
+  example 'Question with other iri prefix' do
+    as :guest, location: '/argu/q/41'
+    wait_for(page).to have_content('Fg question title 7end')
+    expect(page).to have_current_path('/argu/q/41')
+    change_iri_prefix('demogemeente.localdev')
+    click_link('Freetown')
+    # @todo this should be fixed
+    wait_for(page).to have_content('Problem with the browser')
+
+    visit "https://argu.localtest/argu/q/41"
+    wait_for(page).to have_content('Fg question title 7end')
+    expect(page).to have_current_path('/q/41')
+
+    visit "https://demogemeente.localdev/q/41"
+    wait_for(page).to have_content('Fg question title 7end')
+    expect(page).to have_current_path('/q/41')
+  end
+
+  private
+
+  def change_iri_prefix(iri_prefix)
+    rails_runner(
+      :argu,
+      'Apartment::Tenant.switch(\'argu\') do '\
+        "Page.find_via_shortname(\'argu\').update(iri_prefix: '#{iri_prefix}') "\
+      'end'
+    )
+  end
+end
