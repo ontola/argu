9958db8a5a1540b7337b9dffbaa0f9e2aa4ce8e8
Arthur Dingemans <arthur@argu.co>
2021-03-31 09:50:04 +0200

    core#645 Change collection selector

--- spec/features/collection_spec.rb
+++ spec/features/collection_spec.rb
@@ -17,10 +17,11 @@ RSpec.describe 'Collection', type: :feature do
     as :guest, location: '/argu/feed?type=paginated'
     wait_for(page).to have_content('Activities')
     wait_until_loaded
-    wait_for { resource_selector(page.current_url) }.to have_css('.Card', count: 10)
+    collection_iri = page.current_url
+    wait_for { resource_selector(collection_iri) }.to have_css('.Card', count: 10)
     expect(page).not_to have_content('Load more')
     click_button '2'
     wait_until_loaded
-    wait_for { resource_selector(page.current_url) }.to have_css('.Card', count: 9)
+    wait_for { resource_selector(collection_iri) }.to have_css('.Card', count: 9)
   end
 end
